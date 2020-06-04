7b1a83e8b21f7c873cd0f62ae721ec7d52baf644
priteeshh <priteeshh@gmail.com>
2020-06-04 23:44:09 +0530

    delete card functionality implemented

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,9 +1,16 @@
 var deleteForm = document.getElementById("modal-delete");
-
-function onDeleteTapped(){
+var toBeDeleatedCard;
+function onDeleteTapped(selectedCard){
+    toBeDeleatedCard = document.getElementById(selectedCard);
+    console.log(selectedCard)
+    document.getElementById(selectedCard)
     deleteForm.style.display = "block";
 }
 function noBtnTapped(){
     deleteForm.style.display = "none";
 
+}
+function yesBtnTapped(){
+    toBeDeleatedCard.style.display = "none";
+    deleteForm.style.display = "none";
 }
