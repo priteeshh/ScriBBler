71da82350f277d1aa875724b29ceef50f1a8657e
priteeshh <priteeshh@gmail.com>
2020-06-04 22:49:07 +0530

    common java script added and designed delete modal

---
+++ scripts/postslist.js
@@ -0,0 +1,9 @@
+var deleteForm = document.getElementById("modal-delete");
+
+function onDeleteTapped(){
+    deleteForm.style.display = "block";
+}
+function noBtnTapped(){
+    deleteForm.style.display = "none";
+
+}
